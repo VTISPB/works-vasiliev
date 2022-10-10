af300d39d16419aae102863e78dadb8a79a250e2
VTISPB <87233917+VTISPB@users.noreply.github.com>
2022-10-10 18:37:02 +0300

    01intro

---
+++ 01intro/print.py
@@ -0,0 +1,11 @@
+print('Hello World!')
+
+print('Hello', 'World', end='! ')
+print(1, 2, 3, sep='-')
+
+age = 22
+name = 'Nick'
+print('My name is', name, "and I'm", age)
+
+name = input("Как Вас зовут? ")
+print("Очень приятно,", name)

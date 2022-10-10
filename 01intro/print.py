814f3068fd02e447056f831fa8456ed33562d852
VTISPB <87233917+VTISPB@users.noreply.github.com>
2022-10-10 18:56:23 +0300

    print

--- 01intro/print.py
+++ 01intro/print.py
@@ -9,3 +9,27 @@ print('My name is', name, "and I'm", age)
 
 name = input("Как Вас зовут? ")
 print("Очень приятно,", name)
+age = input('Сколько Вам лет? ')
+age = int(age)
+print('Через 10 лет Вам будет', age + 10)
+
+print(5 + 3)
+print(8 - 1)
+print(6 * 4)
+print(3 / 5)
+print(13 // 3)
+print(13 % 3)
+print(9 ** 2)
+print(+4)
+print(-(5 + 3))
+
+a = 4
+a *= 3
+a += 3
+a -= 5
+a //= 2  
+a **= 2
+a %= 3
+a /= 2
+
+print(a)
